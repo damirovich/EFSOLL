8e7c98bca616bbec4e09129095b94a41f26b8813
baktilekashyrov <baktilekashyrov8@gmail.com>
2021-05-24 09:41:21 +0600

    ыы

--- 005Task4/Program.cs
+++ 005Task4/Program.cs
@@ -10,14 +10,43 @@ namespace _005Task4
     {
         static void Main(string[] args)
         {
-            int PayEmpl;
+            double PayEmpl;
             Console.WriteLine("Введите заработной платы сотрудника");
-            int payment = int.Parse(Console.ReadLine());
+            double payment = double.Parse(Console.ReadLine());
             Console.WriteLine("Введите выслуга лет сотрудника");
             int work = int.Parse(Console.ReadLine());
-            if (work < 5) {
+            if (work < 5)
+            {
                 PayEmpl = (payment * 10) / 100;
-                Console.WriteLine("Премия этого сотрудника составляет:" + PayEmpl);
+                Console.WriteLine("Премия этого сотрудника составляет:" + PayEmpl + " Сом");
+                Console.WriteLine("Процент премии 10% от зарабатной платы");
+            }
+            else if (work < 10)
+            {
+                PayEmpl = (payment * 15) / 100;
+                Console.WriteLine("Премия этого сотрудника составляет:" + PayEmpl + " Сом");
+                Console.WriteLine("Процент премии 15% от зарабатной платы");
+            }
+            else if (work < 15)
+            {
+                PayEmpl = (payment * 25) / 100;
+                Console.WriteLine("Премия этого сотрудника составляет:" + PayEmpl + " Сом");
+                Console.WriteLine("Процент премии 25% от зарабатной платы");
+            }
+            else if (work < 20)
+            {
+                PayEmpl = (payment * 35) / 100;
+                Console.WriteLine("Премия этого сотрудника составляет:" + PayEmpl + " Сом");
+                Console.WriteLine("Процент премии 35% от зарабатной платы");
+            }
+            else if (work < 25)
+            {
+                PayEmpl = (payment * 45) / 100;
+                Console.WriteLine("Премия этого сотрудника составляет:" + PayEmpl + " Сом");
+                Console.WriteLine("Процент премии 45% от зарабатной платы");
+            }
+            else {
+                Console.WriteLine("Ошибка!");
             }
             Console.ReadKey();
         }
